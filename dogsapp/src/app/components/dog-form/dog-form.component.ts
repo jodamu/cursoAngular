1c3bb32b09b6c1f4ecfdac9ea2a5620a0fe2a650
Jose Daniel <jodamu90@gmail.com>
2025-06-17 08:10:49 -0600

    finalizacion proyecto dogsapp

--- dogsapp/src/app/components/dog-form/dog-form.component.ts
+++ dogsapp/src/app/components/dog-form/dog-form.component.ts
@@ -1,64 +1,66 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Dog, DogService } from '../../services/dog.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { take } from 'rxjs';
-import { Router } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router'
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dog-form',
-  imports: [ReactiveFormsModule, MatCardModule, MatInputModule,MatFormFieldModule,MatButtonModule],
+  imports: [ 
+    ReactiveFormsModule, 
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+   ],
   templateUrl: './dog-form.component.html',
   styleUrl: './dog-form.component.css'
 })
 export class DogFormComponent {
-  private service:DogService = inject(DogService);
-  private build=inject(FormBuilder);
-  private router=inject(Router);
-  form = this.build.group({
-    breed:['',[
-                Validators.required, 
-                Validators.maxLength(150),
-                Validators.pattern('/^[A-Z,a-z,\s]/')
-              ]],
-    description:['',[
-                Validators.required, 
-                Validators.minLength(5),
-                Validators.maxLength(200),
-                Validators.pattern('/^[A-Z,a-z,\s]/')
-              ]],
-    urlImage:['',[
-                Validators.required,
-                Validators.maxLength(200),
-                Validators.pattern(/^https?:\/\/[^\s]+?\.(jpg|jpeg|png)$/)
-              ]]
+  private service:DogService=inject(DogService);
+  private build=inject(FormBuilder)
+  private router=inject(Router)
+
+  form=this.build.group({
+    breed: ['', [
+      Validators.required, 
+      Validators.maxLength(50), 
+      Validators.pattern(/^[a-z,A-Z,\s]/)]],
+    
+    description: ['', [
+        Validators.required, 
+        Validators.maxLength(200), 
+        Validators.pattern(/^[a-z,A-Z,\s]/)]],
+    
+    urlImage: ['', [
+      Validators.pattern(/^https?:\/\/[^\s]+?\.(jpg|jpeg|png)$/),
+      Validators.required,
+      Validators.maxLength(200)
+    ]]
   });
 
   submit(){
-    if(this.form.valid){
-      let breed=this.form.controls.breed.value!;
-      let description=this.form.controls.description.value!;
-      let urlImage=this.form.controls.urlImage.value!;
+    let breed=this.form.controls.breed.value!;
+    let description=this.form.controls.description.value!;
+    let urlImage=this.form.controls.urlImage.value!;
 
-      let dog:Dog={
-        id:0,
-        breed:breed,
-        description:description,
-        urlImage:urlImage
-      };
-      this.service.insertDog(dog).pipe(take(1)).subscribe({
-        next: value=>{this.router.navigate(['dogs'])},
-        error: err => {console.error(err)}
-      })
+    let dog:Dog={
+      id:0,
+      breed:breed,
+      description:description,
+      urlImage:urlImage
+    }
 
-   
-
-      }
-      
+    this.service.insertDog(dog).pipe(take(1)).subscribe({
+      next: value => {
+        this.router.navigate(['dogs'])
+      },
+      error: err=>{console.log(err)}
+    });
   }
 
 }
-
--- dogsapp/src/app/services/dog.service.ts
+++ dogsapp/src/app/services/dog.service.ts
@@ -10,57 +10,55 @@ export interface Dog{
   urlImage:string
 }
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
-  private http: HttpClient = inject(HttpClient);
-  private baseUri = 'http://localhost:8082/dog';
+  private http:HttpClient=inject(HttpClient);
+  private baseUri:string="http://localhost:8082/dog";
 
+  /*get dogs from api */
+  getDogs():Observable<Dog[]>{
+     return this.http.get<any[]>(this.baseUri)
+     .pipe(
+        timeout(3000), 
+        map(anyDataList => anyDataList.map(any => any as Dog) ),
+        catchError(err=>{
+          console.log("error al obtener api data ", err);
+          return throwError(()=>new Error("error get api data"));
+        }) 
+     );
+  }
+  
 
-  getDogs(): Observable<Dog[]> {
-    return this.http.get<any[]>(this.baseUri)
-      .pipe(timeout(3000),
-        map(anyDataList => anyDataList.map(any => any as Dog)),
-        catchError(err => {
-          console.error('Error al obtener api data', err);
-          return throwError(() => new Error("error get api data"))
-        }
-        )
-      );
+  /*insert dog into api*/
+  insertDog(dog:Dog):Observable<void>{
+     return this.http.post<void>(this.baseUri, dog)
+            .pipe(
+              timeout(3000),
+              catchError(err=>{
+                console.log(" error insert ", err);
+                return throwError(()=> new Error(" error al insertar "))
+              })
+            );
   }
 
-  insertDog(dog: Dog): Observable<void> {
-    return this.http.post<void>(this.baseUri + '/add', dog)
-      .pipe(timeout(3000),
-        catchError(err => {
-          console.error('Error al insertar dog', err);
-          return throwError(() => new Error("error insert dog"))
-        }
-        )
-      );
+
+  /*delete dog from api */
+  deleteDog(id:number):Observable<void>{
+     return this.http.delete<void>(`${this.baseUri}/${id}`)
+     .pipe(
+      timeout(3000),
+      catchError(err=>{
+        console.log(" error delete ", err);
+        return throwError(()=> new Error(" error al eliminar "))
+      })
+     );
   }
 
-  updateDog(dog: Dog): Observable<void> {
-    return this.http.put<void>(this.baseUri + '/', dog)
-      .pipe(timeout(3000),
-        catchError(err => {
-          console.error('Error al actualizar dog', err);
-          return throwError(() => new Error("error update dog"))
-        }
-        )
-      );
-  }
 
-  deleteDog(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUri}/${id}`)
-      .pipe(timeout(3000),
-        catchError(err => {
-          console.error('Error al eliminar dog', err);
-          return throwError(() => new Error("error delete dog"))
-        }
-        )
-      );
-  }
 
+
+  
 }
